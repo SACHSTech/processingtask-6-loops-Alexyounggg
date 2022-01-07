fbd2787dd59f92be42a82e2b44f356df744b6a25
Alexyounggg <alex.young23@ycdsbk12.ca>
2022-01-07 18:31:29 -0500

    Submit

--- Sketch.java
+++ Sketch.java
@@ -1,6 +1,8 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
+
+ 
 	
 	
   /**
@@ -8,7 +10,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(750, 750);
   }
 
   /** 
@@ -24,13 +26,48 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    //Quad One
 
+    for (int circleY = height/40; circleY < height/2; circleY += height/2/5){
+      for (int circleX = width/18 * 17; circleX > width/2; circleX -= width/2/5){
+        fill(255);
+        ellipse(circleX, circleY, width/40, height/40);
+      }
+    }
+    
+
+
+    //Quad Two
     stroke(255);
-    line(50, 125, 70, 50);  
+    for (int lineX = 0; lineX < width/2; lineX += width/2/10){
+      line(lineX, 0, lineX, height/2);
+    }
+    for (int lineY = 0; lineY < height/2; lineY += height/2/10){
+      line(0, lineY, width/2, lineY);
+    }
+
+    //Quad Three
+    for (int i = 0; i < width/2; i++){
+      stroke(i, i , i);
+      line(i, height/2, i, height);
+    }
+
+    // Quad Four
+
+    for (int j = 0; j < 136; j+=45){
+      pushMatrix();
+      translate(width/4*3, height/4*3);
+      rotate(radians(j));
+      ellipse(0, 0, width/20, height/3);
+      popMatrix();
+    }
+    
+    fill (255, 255, 0);
+    ellipse(width/4*3, height/4*3, width/10, height/10);
+   
+   
+    
   }
   
-  // define other methods down here.
+
 }
